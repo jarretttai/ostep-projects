e8372bde587f58d6e4faf557459675fcb1709819
jarretttai <jarrett0699@outlook.com>
2021-12-20 14:32:29 -0800

    Finished wcat

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+    for (int i = 1; i < argc; ++i) {
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("wcat: cannot open file\n");
+            return 1;
+        }
+        char *line = malloc(LINE_MAX + 1);
+        while (fgets(line, LINE_MAX + 1, fp) != NULL) {
+            printf("%s", line);
+        }
+    }
+    
+    
+    return 0;
+}
